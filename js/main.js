9879672bf77c184a9b3c8c3fd33cd29ac9ebe463
Mamunur Rashid Mamun <mrmamun20162017@gmail.com>
2021-09-15 20:20:34 +0600

    Client Feedback added

--- js/main.js
+++ js/main.js
@@ -22,4 +22,22 @@ $(document).ready(function() {
         type: 'image',
         gallery: { enabled: true }
     });
+
+
+    // Owl-Carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            544:{
+                items: 2
+            }
+        }
+    });
+
 });
